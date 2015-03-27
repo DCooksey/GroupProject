b9fdce3401aa8a9af7552e1225bdcb3fa215dc2b
DCooksey <dean.g.cooksey@googlemail.com>
2015-03-27 11:36:05 +0000

    reup

--- loginhandler.php
+++ loginhandler.php
@@ -2,48 +2,34 @@
     session_start();
     $dbcon = mysqli_connect("igor.gold.ac.uk", "ma202dc", "emigrmnt", "ma202dc_GroupProject");
 		
-	if($dbcon == FALSE){
-		die("ERROR: Could not connect to database. " . mysqli_connect_error());
-	}
-		
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	
-	if($email == ""){
-		echo 'Login Failed!';
+	if($dbcon == FALSE) {
+		print "ERROR: Could not connect to database. " . mysqli_connect_error();
 	}
 	
-	$email = mysqli_real_escape_string($dbcon, $email);
-	$password = mysqli_real_escape_string($dbcon, $password);
-	
-	if(isset($_POST['submitLogin'])){
+	if(!empty($_POST['email']) && !empty($_POST['password'])) {
 		
-		$query = "SELECT ID, Email, Password FROM User WHERE Email='$email' AND Password='$password";
+		$email = mysqli_real_escape_string($dbcon, $_POST['email']);
+		$password = mysqli_real_escape_string($dbcon, $_POST['password']);
+		
+		$query = "SELECT * FROM User WHERE Email='$email' AND Password='$password'";
 		
 		$result = mysqli_query($dbcon, $query);
 		
-		$row = mysqli_fetch_row($result);
+		$row = mysqli_fetch_array($result);
 		
-		//$id = $row[0];
-		
-		//$dbEmail= $row[1];
-		
-		//$dbPassword = $row[2];
-		
-		//if($email== $dbEmail && $password == $dbPassword){
-			
-			
-
-		if($result){
+		if($row['Email'] == $email && $row['Password'] == $password){
 		$_SESSION['currentUser'] = $email;
 		header('location: index.php');
 		
 		}
 		else {
-			echo 'Login Failed!' .$dbEmail.$dbPassword;
+			echo 'Login Failed!';
 			
 		}
 		
 		mysqli_close($dbcon);
 	}
+		
+		
+
 ?>
